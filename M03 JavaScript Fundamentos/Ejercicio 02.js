ce14f6ae8e87e9e258b3a9b501dbfd94b9a3a363
CsalinasA <arqcarlosalinas25@gmail.com>
2023-08-15 23:40:29 -0500

    2 essercizio completo

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -11,29 +11,34 @@ devolverString('hola')
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
-   // Retorna el resultado de su suma.
-   // Tu código:
+   var sumacion = x + y// Retorna el resultado de su suma.
+   return sumacion// Tu código:
 }
+suma(28, 30)
 
 function resta(x, y) {
-   // Retorna el resultado de la resta.
-   // Tu código:
+   var restacion = x - y // Retorna el resultado de la resta.
+   return restacion // Tu código:
 }
+resta(100, 1)
 
 function divide(x, y) {
-   // Retorna el resultado de su división.
-   // Tu código:
+   var dividelos = x / y // Retorna el resultado de su división.
+   return dividelos// Tu código:
 }
+divide (14, 2)
 
 function multiplica(x, y) {
-   // Retorna el resultado de su multiplicación.
-   // Tu código:
+   var multiplicadera = x * y // Retorna el resultado de su multiplicación.
+   return multiplicadera// Tu código:
 }
+multiplica(10, 4)
 
 function obtenerResto(x, y) {
-   // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+   var restante = x % y // Obten el resto de la división de "x" entre "y".
+   return restante// Tu código:
 }
+obtenerResto(42, 5)
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
